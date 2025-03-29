d2629bc7a76b61b28be1fd24d447d86200323560
kwamevaughan <kwamevaughan@gmail.com>
2025-03-29 17:33:49 +0000

    beginning of time

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Project</h1>
+      <p>This is a dummy index.js file for testing Next.js setup.</p>
+    </div>
+  );
+};
+
+export default HomePage;
