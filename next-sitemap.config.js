f2b6d2e586070908593ceb62083bd05d2c17e0d6
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-04-23 08:17:35 +0000

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: "https://www.paan.africa",
+  generateRobotsTxt: true, // Generates robots.txt
+  sitemapSize: 7000,
+  exclude: ["/thank-you"], // Exclude thank-you page
+};
