688e8c870dd6f09a9744beabe7fc7d3f73b1e0db
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-05-23 08:34:08 +0000

    setup redirect from www to non-www

--- next.config.js
+++ next.config.js
@@ -1,20 +1,36 @@
 // Import the necessary modules
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development', // Disable in development to avoid caching issues
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // Disable in development to avoid caching issues
 });
 
-const initializeBundleAnalyzer = require('@next/bundle-analyzer');
+const initializeBundleAnalyzer = require("@next/bundle-analyzer");
 
 // Configure the Bundle Analyzer
 const withBundleAnalyzer = initializeBundleAnalyzer({
-  enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true',
+  enabled: process.env.BUNDLE_ANALYZER_ENABLED === "true",
 });
 
 // The base Next.js configuration
 const nextConfig = {
-  output: 'standalone',   // Keeps the build output as standalone (recommended for deployment)
-  devIndicators: false,   // Disables the development indicators, including the Next.js logo
+  output: "standalone", // Keeps the build output as standalone (recommended for deployment)
+  devIndicators: false, // Disables the development indicators, including the Next.js logo
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.paan.africa",
+          },
+        ],
+        destination: "https://paan.africa/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 // Export the configuration by combining PWA and Bundle Analyzer
