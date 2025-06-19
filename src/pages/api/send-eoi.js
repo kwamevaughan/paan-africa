d5006a10cdae4e18bf36531c0084feb9a925b88f
Ronald <matekwaronald@gmail.com>
2025-06-19 05:37:39 +0300

    Added Thank you page

--- src/pages/api/send-eoi.js
+++ src/pages/api/send-eoi.js
@@ -193,7 +193,9 @@ export default async function handler(req, res) {
     console.log('Email sent successfully');
     res.status(200).json({ 
       message: 'Expression of Interest submitted successfully!',
-      filesUploaded: attachments.length
+      name,
+      agencyName,
+      country,
     });
   } catch (error) {
     console.error('Error processing form:', error);
