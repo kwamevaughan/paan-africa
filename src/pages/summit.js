0c3ac230c487bf87a602f58fc45e480919f9ba15
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-08-20 11:12:22 +0000

    replaced svg with iconify

--- src/pages/summit.js
+++ src/pages/summit.js
@@ -2,6 +2,7 @@ import SEO from "@/components/SEO";
 import Header from "../layouts/summit-header";
 import Image from "next/image";
 import BreakoutSessions from "@/components/BreakoutSessions";
+import { Icon } from "@iconify/react";
 import Footer from "@/layouts/footer";
 import { useEffect, useRef, useState } from "react";
 import BenefitsToggle from "@/components/BenefitsToggle";
@@ -177,7 +178,7 @@ const SummitPage = () => {
                   src="/assets/images/icon-1.png"
                   width={60}
                   height={60}
-                  alt="Pan-African Reach"
+                  alt="Keynotes & Showcases"
                 />
                 <div>
                   <h4 className="text-sm text-red-500 font-bold mb-2">Keynotes & Showcases</h4>
@@ -191,7 +192,7 @@ const SummitPage = () => {
                   src="/assets/images/icon-2.png"
                   width={60}
                   height={60}
-                  alt="Pan-African Reach"
+                  alt="Networking Opportunities"
                 />
                 <div>
                   <h4 className="text-sm text-red-500 font-bold mb-2">Networking Opportunities</h4>
@@ -205,7 +206,7 @@ const SummitPage = () => {
                   src="/assets/images/icon-3.png"
                   width={60}
                   height={60}
-                  alt="Pan-African Reach"
+                  alt="Workshops & Panels"
                 />
                 <div>
                   <h4 className="text-sm text-red-500 font-bold mb-2">Workshops & Panels</h4>
@@ -395,14 +396,12 @@ const SeminarLocationAndDate = ()=> {
   return (
     <div className="flex md:flex-row flex-col gap-4">
       <div className="flex items-center gap-2 text-white text-sm">
-      <svg xmlns="http://www.w3.org/2000/svg" className="text-red-500" width="24" height="24" viewBox="0 0 24 24"><g fill="none">
-        <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="M12 2a9 9 0 0 1 9 9c0 3.074-1.676 5.59-3.442 7.395a20.4 20.4 0 0 1-2.876 2.416l-.426.29l-.2.133l-.377.24l-.336.205l-.416.242a1.87 1.87 0 0 1-1.854 0l-.416-.242l-.52-.32l-.192-.125l-.41-.273a20.6 20.6 0 0 1-3.093-2.566C4.676 16.589 3 14.074 3 11a9 9 0 0 1 9-9m0 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6"/></g></svg>
+<Icon icon="mdi:map-marker" className="text-red-500" width="24" height="24" />
         <span>Nairobi, Kenya</span>
       </div>
       
       <div className="flex items-center gap-2 text-white text-sm">
-      <svg xmlns="http://www.w3.org/2000/svg" className="text-red-500" width="24" height="24" viewBox="0 0 24 24"><g fill="none">
-        <path fill="currentColor" d="M2 9c0-1.886 0-2.828.586-3.414S4.114 5 6 5h12c1.886 0 2.828 0 3.414.586S22 7.114 22 9c0 .471 0 .707-.146.854C21.707 10 21.47 10 21 10H3c-.471 0-.707 0-.854-.146C2 9.707 2 9.47 2 9m0 9c0 1.886 0 2.828.586 3.414S4.114 22 6 22h12c1.886 0 2.828 0 3.414-.586S22 19.886 22 18v-5c0-.471 0-.707-.146-.854C21.707 12 21.47 12 21 12H3c-.471 0-.707 0-.854.146C2 12.293 2 12.53 2 13z"/><path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M7 3v3m10-3v3"/></g></svg>
+<Icon icon="mdi:calendar" className="text-red-500" width="24" height="24" />
         <span>22-24 October 2025</span>
       </div>
     </div>
