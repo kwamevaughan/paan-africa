b5c3f7ceccaf34276d357853f41acae85b5db82e
Ronald <matekwaronald@gmail.com>
2025-09-24 11:06:56 +0300

    Updated speakers

--- src/pages/summit.js
+++ src/pages/summit.js
@@ -140,45 +140,45 @@ const SummitPage = () => {
     },
     {
       id: 3,
-      name: "Magdalene Kariuki",
-      title: "Associate director, Africa Practice. Kenya",
-      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/Magdalene.jpeg",
-      linkedin: "https://www.linkedin.com/in/magdalene-kariuki-mppa"
-    },
-    {
-      id: 4,
-      name: "Clive Mishon",
-      title: "Founder, Alliance of Independent Agencies. UK",
-      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/clive.jpeg",
-      linkedin: "https://www.linkedin.com/in/clive-mishon-33b5318?originalSubdomain=uk"
-    },
-    {
-      id: 5,
-      name: "Victor Simba",
-      title: "International Development, PWC. Kenya",
-      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/Victor.jpeg",
-      linkedin: "https://www.linkedin.com/in/victor-simba-06502123a?originalSubdomain=ke"
-    },
-    {
-      id: 6,
       name: "Dr. Gillian Hammah",
       title: "CMO Aya Data. Ghana",
       image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/Hammar.jpeg",
       linkedin: "https://www.linkedin.com/in/gillian-hammah?originalSubdomain=gh"
     },
     {
-      id: 7,
-      name: "Musa Kalenga",
-      title: "CEO, Brave Group, South Africa",
-      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/musa.webp",
-      linkedin: "https://www.linkedin.com/in/musa-kalenga-31b10523/?originalSubdomain=za"
-    },
-    {
-      id: 8,
+      id: 4,
       name: "Craig Rodney",
       title: "Founder, The Agency Coach, South Africa",
       image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/Craig.jpeg",
       linkedin: "https://www.linkedin.com/in/craigrodney/?originalSubdomain=za"
+    },
+    {
+      id: 5,
+      name: "Clive Mishon",
+      title: "Founder, Alliance of Independent Agencies. UK",
+      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/clive.jpeg",
+      linkedin: "https://www.linkedin.com/in/clive-mishon-33b5318?originalSubdomain=uk"
+    },
+    {
+      id: 6,
+      name: "Magdalene Kariuki",
+      title: "Associate director, Africa Practice. Kenya",
+      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/Magdalene.jpeg",
+      linkedin: "https://www.linkedin.com/in/magdalene-kariuki-mppa"
+    },
+    {
+      id: 7,
+      name: "Victor Simba",
+      title: "International Development, PWC. Kenya",
+      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/Victor.jpeg",
+      linkedin: "https://www.linkedin.com/in/victor-simba-06502123a?originalSubdomain=ke"
+    },
+    {
+      id: 8,
+      name: "Musa Kalenga",
+      title: "CEO, Brave Group, South Africa",
+      image: "https://ik.imagekit.io/nkmvdjnna/PAAN/summit/speakers/musa.webp",
+      linkedin: "https://www.linkedin.com/in/musa-kalenga-31b10523/?originalSubdomain=za"
     },
   ];
 
