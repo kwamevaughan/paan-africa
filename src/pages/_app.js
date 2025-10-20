20890ebd8eafdbdba8b4ccc5565349a1461ab512
Ronald <matekwaronald@gmail.com>
2025-10-20 11:07:08 +0300

    Added apollo tracking script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -208,7 +208,7 @@ function MyApp({ Component, pageProps }) {
 
       <Toaster position="top-right" toastOptions={{
         duration: 4000,
-        style: { background: '#363636', color: '#fff' },
+        style: { background: '#172840', color: '#fff' },
         success: { duration: 3000, iconTheme: { primary: '#4ade80', secondary: '#fff' } },
         error: { duration: 5000, iconTheme: { primary: '#ef4444', secondary: '#fff' } },
       }}/>
