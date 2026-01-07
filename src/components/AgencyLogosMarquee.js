9ebd53d470a84d154d32175da47275e5507f5956
Ronald <matekwaronald@gmail.com>
2026-01-07 17:35:15 +0300

    Updated the homepage UI with more agency logos

--- src/components/AgencyLogosMarquee.js
+++ src/components/AgencyLogosMarquee.js
@@ -4,8 +4,8 @@ import Image from 'next/image';
 import styles from './AgencyLogosMarquee.module.css';
 
 const AgencyLogosMarquee = () => {
-  // Agency logos array
-  const agencyLogos = [
+  // Agency logos array for left-sliding marquee
+  const agencyLogosLeft = [
     {
       id: 1,
       name: 'Aquila East Africa',
@@ -26,20 +26,68 @@ const AgencyLogosMarquee = () => {
       name: 'Nitro 21',
       logo: '/assets/images/agencies/nitro-21.png',
     },
+  ];
+
+  // Agency logos array for right-sliding marquee
+  const agencyLogosRight = [
     {
       id: 5,
-      name: 'Omni Options',
-      logo: '/assets/images/agencies/omni-options.avif',
+      name: '1117',
+      logo: '/assets/images/agencies/free-members/1117-logo-website.png',
     },
     {
       id: 6,
-      name: 'Penguin Agency',
-      logo: '/assets/images/agencies/penquin.png',
+      name: 'Alien Nation',
+      logo: '/assets/images/agencies/free-members/alien-nation-white.png',
+      isWhite: true,
     },
     {
       id: 7,
-      name: 'Xperia-agency',
-      logo: '/assets/images/agencies/Xperia-agency.png',
+      name: 'Amethyst',
+      logo: '/assets/images/agencies/free-members/amethyst.png',
+    },
+    {
+      id: 8,
+      name: 'Carillons Marketing',
+      logo: '/assets/images/agencies/free-members/carillons.ma.png',
+    },
+    {
+      id: 9,
+      name: 'fisheye',
+      logo: '/assets/images/agencies/free-members/fisheye.svg',
+    },
+    {
+      id: 10,
+      name: 'Isel',
+      logo: '/assets/images/agencies/free-members/isel-BLACK-LOGO.png',
+    },
+    {
+      id: 11,
+      name: 'KWETU Marketing',
+      logo: '/assets/images/agencies/free-members/KWETU.png',
+      isWhite: true,
+    },
+    {
+      id: 12,
+      name: 'Lebu Media',
+      logo: '/assets/images/agencies/free-members/Lebu-Logo-Dark-BG.png',
+      isWhite: true,
+    },
+    {
+      id: 13,
+      name: 'ONYX AFRICA',
+      logo: '/assets/images/agencies/free-members/ONYXAFRICAvectorblack.jpg',
+    },
+    {
+      id: 14,
+      name: 'Softlitic',
+      logo: '/assets/images/agencies/free-members/Softlitic.avif',
+    },
+    {
+      id: 15,
+      name: 'Zilojo',
+      logo: '/assets/images/agencies/free-members/zilojo-small-logo-white.png',
+      isWhite: true,
     },
   ];
 
@@ -53,28 +101,29 @@ const AgencyLogosMarquee = () => {
             alt={agency.name}
             width={120}
             height={60}
-            className={styles.logo}
+            className={`${styles.logo} ${agency.isWhite ? styles.logoInverted : ''}`}
           />
         </div>
       </div>
     ));
 
   // Duplicate logos for seamless loop
-  const logosDuplicated = [...agencyLogos, ...agencyLogos, ...agencyLogos];
+  const logosLeftDuplicated = [...agencyLogosLeft, ...agencyLogosLeft, ...agencyLogosLeft];
+  const logosRightDuplicated = [...agencyLogosRight, ...agencyLogosRight, ...agencyLogosRight];
 
   return (
     <section className={styles.section}>
       {/* Top Marquee - Moving Left */}
       <div className={styles.marqueeContainer}>
         <div className={`${styles.marquee} ${styles.marqueeLeft}`}>
-          {renderLogoItems(logosDuplicated)}
+          {renderLogoItems(logosLeftDuplicated)}
         </div>
       </div>
 
       {/* Bottom Marquee - Moving Right */}
       <div className={styles.marqueeContainer}>
         <div className={`${styles.marquee} ${styles.marqueeRight}`}>
-          {renderLogoItems(logosDuplicated)}
+          {renderLogoItems(logosRightDuplicated)}
         </div>
       </div>
     </section>
