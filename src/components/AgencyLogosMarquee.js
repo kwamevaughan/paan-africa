2800054f1060520781ad2a5232efe68ca62cae4f
Ronald <matekwaronald@gmail.com>
2026-02-11 12:54:09 +0300

    Added more agency logos

--- src/components/AgencyLogosMarquee.js
+++ src/components/AgencyLogosMarquee.js
@@ -26,68 +26,113 @@ const AgencyLogosMarquee = () => {
       name: 'Nitro 21',
       logo: '/assets/images/agencies/nitro-21.png',
     },
+    {
+      id: 5,
+      name: 'Penquin',
+      logo: '/assets/images/agencies/penquin.png',
+    },
+    {
+      id: 6,
+      name: 'Xperia Agency',
+      logo: '/assets/images/agencies/Xperia-agency.png',
+    },
+    {
+      id: 7,
+      name: 'Omni Options',
+      logo: '/assets/images/agencies/omni-options.avif',
+    },
   ];
 
   // Agency logos array for right-sliding marquee
   const agencyLogosRight = [
     {
-      id: 5,
+      id: 20,
       name: '1117',
       logo: '/assets/images/agencies/free-members/1117-logo-website.png',
     },
     {
-      id: 6,
+      id: 21,
       name: 'Alien Nation',
       logo: '/assets/images/agencies/free-members/alien-nation-white.png',
       isWhite: true,
     },
     {
-      id: 7,
+      id: 22,
       name: 'Amethyst',
       logo: '/assets/images/agencies/free-members/amethyst.png',
     },
     {
-      id: 8,
+      id: 23,
       name: 'Carillons Marketing',
       logo: '/assets/images/agencies/free-members/carillons.ma.png',
     },
     {
-      id: 9,
+      id: 24,
       name: 'fisheye',
       logo: '/assets/images/agencies/free-members/fisheye.svg',
     },
     {
-      id: 10,
+      id: 25,
       name: 'Isel',
       logo: '/assets/images/agencies/free-members/isel-BLACK-LOGO.png',
     },
     {
-      id: 11,
+      id: 26,
       name: 'KWETU Marketing',
       logo: '/assets/images/agencies/free-members/KWETU.png',
       isWhite: true,
     },
     {
-      id: 12,
+      id: 27,
       name: 'Lebu Media',
       logo: '/assets/images/agencies/free-members/Lebu-Logo-Dark-BG.png',
       isWhite: true,
     },
     {
-      id: 13,
+      id: 28,
       name: 'ONYX AFRICA',
       logo: '/assets/images/agencies/free-members/ONYXAFRICAvectorblack.jpg',
     },
     {
-      id: 14,
+      id: 29,
       name: 'Softlitic',
       logo: '/assets/images/agencies/free-members/Softlitic.avif',
     },
     {
-      id: 15,
+      id: 30,
       name: 'Zilojo',
       logo: '/assets/images/agencies/free-members/zilojo-small-logo-white.png',
       isWhite: true,
+    },
+    {
+      id: 31,
+      name: 'Recruitement Room',
+      logo: '/assets/images/agencies/free-members/recruitement-room.png',
+      isWhite: false,
+    },
+    {
+      id: 32,
+      name: 'African Elite Group',
+      logo: '/assets/images/agencies/free-members/african-elite-group.webp',
+      isWhite: true,
+    },
+    {
+      id: 33,
+      name: 'Research 8020',
+      logo: '/assets/images/agencies/free-members/reserch-8020.png',
+      isWhite: false,
+    },
+    {
+      id: 34,
+      name: 'Nibble Africa',
+      logo: '/assets/images/agencies/free-members/nibble-africa.png',
+      isWhite: false,
+    },
+    {
+      id: 34,
+      name: 'Batou Digital',
+      logo: '/assets/images/agencies/free-members/batou-digital.webp',
+      isWhite: false,
     },
   ];
 
